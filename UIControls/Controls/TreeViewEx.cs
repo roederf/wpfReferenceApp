64ab026994d5ea77ceb79106ea745cfb0c2d4677
Patrik Ramminger <patrik@amplifycamp.com>
2014-01-10 17:18:54 +0100

    new stand

---
+++ UIControls/Controls/TreeViewEx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Sandbox.Controls
+{
+    public class TreeViewEx : TreeView
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new TreeViewItemEx();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is TreeViewItemEx;
+        }
+    }
+}
